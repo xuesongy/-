6d629d78fe1a8a0ef5a60ea5805196dc44125bd0
xuesongy <y1424103536@163.com>
2020-04-15 23:54:27 +0800

    进程和线程

---
+++ 面向对象/DingShiQi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 面向对象 {
+    class DingShiQi {
+        /// 定时器
+        /// 
+        /// DispatcherTimer 类
+        /// 可以实现一些定时操作
+        /// 
+        /// DispatcherTimer 的使用：
+        ///     1. 为 DispatcherTimer 设置一个时间间隔
+        ///     2. 创建 Tick 的事件处理
+        ///     3. 使用 Start 方法开始计时
+        ///     4. Tick 事件根据设置的时间间隔来执行事件处理中的代码
+        /// 
+    }
+}
